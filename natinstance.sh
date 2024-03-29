944d473bb51a2d2d4569cf8fce678ae4d67b3582
ymk94 <157346158+ymk94@users.noreply.github.com>
2024-03-29 14:46:26 +0900

    Update natinstance.sh

--- natinstance.sh
+++ natinstance.sh
@@ -4,4 +4,5 @@ sudo sysctl -p /etc/sysctl.conf
 sudo yum install -y iptables-services
 sudo systemctl start iptables.service
 sudo systemctl enable iptables.service
+sudo iptables -F
 sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
