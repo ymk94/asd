dabab7f69493a04a6f4ef2a36a27f976cfbfeb06
ymk94 <157346158+ymk94@users.noreply.github.com>
2024-04-02 15:59:39 +0900

    Create ping.sh

---
+++ aws/ping.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cat ./list.txt |  while read output
+do
+    ping -c 1 -W 1 "$output" > /dev/null
+    if [ $? -eq 0 ]; then
+    echo "$output is up"
+    else
+    echo "$output is down"
+    fi
+done
